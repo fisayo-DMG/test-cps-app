e998c985dc024cd7523df007a04aa7efb7e119ad
fisayo-DMG <aikomofisayo@gmail.com>
2018-01-18 11:51:56 +0100

    Modified templates to display posts from database.

--- records/views.py
+++ records/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Student
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'records/post_list.html')
+	students = Student.objects.order_by('date_of_birth')
+	return render(request, 'records/post_list.html', {'students': students})
